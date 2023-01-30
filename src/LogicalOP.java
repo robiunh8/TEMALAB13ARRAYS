31c75d524f1d3e01e5e26c497a1b7f6ad70f7bb1
Robert Kerekes <robert.kerekes@yahoo.com>
2023-01-30 14:08:21 +0200

    TEMALAB13ARRAYS

--- src/LogicalOP.java
+++ src/LogicalOP.java
@@ -1,2 +1,114 @@
-package PACKAGE_NAME;public class LogicalOP {
+import java.util.Arrays;
+
+public class LogicalOP {
+
+    public void arrayEX1(){
+        int[] arrayex2 = new int[100];
+        for (int i = 0; i < arrayex2.length; i++) {
+            arrayex2[i] = i + 1;
+        }
+        for (int num : arrayex2) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void populateEvenNumbers(int[] arr) {
+        int index = 0;
+        for (int i = 2; i <= 100; i += 2) {
+            arr[index] = i;
+            index++;
+        }
+    }
+    public static int[] populateArray(int[] array) {
+        int[] numbers = {2, 3, 4, 5};
+        for (int i = 0; i < array.length; i++) {
+            array[i] = numbers[i];
+        }
+        return array;
+    }
+
+    public static double calculateAverage(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return (double) sum / array.length;
+    }
+
+    public static String[] populateArray(String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = "value" + 3;
+        }
+        return array;
+    }
+    public static boolean checkValue(String[] array, String value) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals(value)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int findNumberInArray(int[] arr, int num) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == num) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void printGrid() {
+        String[] grid = new String[100];
+        Arrays.fill(grid, "- ");
+        for (int i = 0; i < grid.length; i++) {
+            System.out.print(grid[i]);
+            if ((i + 1) % 10 == 0) {
+                System.out.println();
+            }
+        }
+    }
+
+    public static int[] removeNumber(int[] array, int numberex7) {
+        int[] newArray = new int[array.length - 1];
+        int j = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != numberex7) {
+                newArray[j++] = array[i];
+            }
+        }
+        return newArray;
+    }
+
+    public static int findSecondSmallest(int[] arr) {
+        int[] fixedArray = {10, 20, 30, 40, 50};
+        Arrays.sort(arr);
+        int secondSmallest = Integer.MAX_VALUE;
+        for (int i : fixedArray) {
+            if (i > arr[0] && i < secondSmallest) {
+                secondSmallest = i;
+            }
+        }
+        return secondSmallest;
+    }
+
+    public static void copyArray(int[] source, int[] destination) {
+        for (int i = 0; i < source.length; i++) {
+            destination[i] = source[i];
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,99 @@
+import java.util.Arrays;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LogicalOP opArray= new LogicalOP();
+
+        //ex1 array
+        System.out.println("EX 1 Array");
+
+        opArray.arrayEX1();
+
+
+        //ex2 array
+        System.out.println(" ");
+        System.out.println("EX 2");
+
+        int[] evenNumbers = new int[50];
+        opArray.populateEvenNumbers(evenNumbers);
+        for(int i=0;i<evenNumbers.length;i++)
+            System.out.print(evenNumbers[i]+" ");
+
+
+        //ex 3 array
+        System.out.println(" ");
+        System.out.println("EX 3");
+
+        int[] numbers = new int[4];
+        numbers = opArray.populateArray(numbers);
+        System.out.println("Average: " + opArray.calculateAverage(numbers));
+
+
+        //ex 4 array
+        System.out.println(" ");
+        System.out.println("EX 4");
+
+        String[] stringArray = new String[2];
+        stringArray = opArray.populateArray(stringArray);
+        String value = "value2";
+        System.out.println(opArray.checkValue(stringArray, value));
+
+        //ex 5 array
+        System.out.println(" ");
+        System.out.println("EX 5");
+
+        int[] array = {1, 2, 3, 4, 5};
+        int num = 3;
+        int result = opArray.findNumberInArray(array, num);
+        if(result == -1)
+            System.out.println(num + " not found in the array");
+        else
+            System.out.println(num + " found at index " + result);
+
+        //ex 6 array
+        System.out.println(" ");
+        System.out.println("EX 6");
+
+        opArray.printGrid();
+
+
+        //ex 7 array
+        System.out.println(" ");
+        System.out.println("EX 7");
+
+        int[] numberex7 = {1, 2, 3, 4, 5};
+        int numberToRemove = 4;
+        numberex7 = LogicalOP.removeNumber(numberex7, numberToRemove);
+        System.out.println(Arrays.toString(numberex7));
+
+
+        //ex 8 array
+        System.out.println(" ");
+        System.out.println("EX 8");
+
+        int[] myArray = {12, 15, 40, 50, 30};
+        int secondSmallest = LogicalOP.findSecondSmallest(myArray);
+        System.out.println(secondSmallest);
+
+
+        // ex 9 array
+        System.out.println(" ");
+        System.out.println("EX 9");
+
+        int[] sourceArray = {10, 100, 1000, 10000};
+        int[] destinationArray = new int[sourceArray.length];
+        System.out.println("Source Array before transfer: "+ Arrays.toString(sourceArray));
+        System.out.println("Destination Array before transfer: "+ Arrays.toString(destinationArray));
+        LogicalOP.copyArray(sourceArray, destinationArray);
+        System.out.println("Source Array after transfer: "+ Arrays.toString(sourceArray));
+        System.out.println("Destination Array after transfer: "+ Arrays.toString(destinationArray));
+
+
+
+
+
+
+
     }
 }
